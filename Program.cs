6edcdecc3d427f1a94b9280a7ecb1b079f7c5dbb
Adrian11Faudoa <afa1823@gmail.com>
2026-03-10 12:51:20 -0600

    Update Program.cs: Template

--- Program.cs
+++ Program.cs
@@ -6,33 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        bool looping = true;
-        while (looping)
-        {
-            try
-            {
-                Console.Write("Enter a number: ");
-                int num = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine(num);
-
-                looping = false;
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine("Please enter a number smaller than 2 billion");
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Please only enter numbers");
-            }
-            catch (Exception) // catch all exceptions
-            {
-                Console.WriteLine("Something has went wrong");
-            }
-        }
-
-        Console.WriteLine("Goodbye!");
-
+        
         Console.ReadLine();
     }
 
