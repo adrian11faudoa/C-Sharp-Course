a0489ad4ee2fa0818bcb284a7cd59fd802227e08
Adrian11Faudoa <afa1823@gmail.com>
2026-03-20 21:03:14 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,27 +7,11 @@ class Program
 {
     class Person
     {
-        //Auto-implemented properties: when no additional logic is needed in the getter and setter
-        //public string Name { get; set; }
-        //public int Age { get; set; }
-
-
         private string name;
         private int age;
 
-        //Properties 
         public string Name
         {
-            /*
-            get 
-            {
-                return name;
-            }
-            set
-            {
-                name = value;
-            }
-             */
             get => name;
             set => name = !string.IsNullOrEmpty(value) ? value : "Invalid name";
         }
@@ -36,18 +20,11 @@ class Program
             get => age;
             set => age = value >= 0 && value <= 150 ? value : -1;
         }
-
         public Person(string name, int age)
         {
             Name = name;
             Age = age;
         }
-
-
-        /*public void SetName(string name) => this.name = !string.IsNullOrEmpty(name) ? name : "Invalid name";
-        public string GetName() => name;
-        public void SetAge(int age) => this.age = age >= 0 && age <= 150 ? age : -1;
-        public int GetAge() => age;*/
         public string ReturnDetails()
         {
             return $"Name: {Name}, Age: {Age}";
@@ -57,8 +34,6 @@ class Program
     {
         Person person = new Person("Alice", 30);
         Console.WriteLine(person.ReturnDetails());
-
-        Console.WriteLine($"Your name is {person.Name}, and your age is {person.Age}");
 
 
         Console.ReadLine();
