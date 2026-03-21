d30b6bbdb38f2f15858d7d6e078d45fb4c1e815c
Adrian11Faudoa <afa1823@gmail.com>
2026-03-20 20:58:19 -0600

    Update Readme.md: Class Properties

--- Program.cs
+++ Program.cs
@@ -7,20 +7,50 @@ class Program
 {
     class Person
     {
+        //Auto-implemented properties: when no additional logic is needed in the getter and setter
+        //public string Name { get; set; }
+        //public int Age { get; set; }
+
+
         private string name;
         private int age;
+
+        //Properties 
+        public string Name
+        {
+            /*
+            get 
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+             */
+            get => name;
+            set => name = !string.IsNullOrEmpty(value) ? value : "Invalid name";
+        }
+        public int Age
+        {
+            get => age;
+            set => age = value >= 0 && value <= 150 ? value : -1;
+        }
+
         public Person(string name, int age)
         {
-            this.name = name;
-            this.age = age;
+            Name = name;
+            Age = age;
         }
-        public void SetName(string name) => this.name = !string.IsNullOrEmpty(name) ? name : "Invalid name";
+
+
+        /*public void SetName(string name) => this.name = !string.IsNullOrEmpty(name) ? name : "Invalid name";
         public string GetName() => name;
         public void SetAge(int age) => this.age = age >= 0 && age <= 150 ? age : -1;
-        public int GetAge() => age;
+        public int GetAge() => age;*/
         public string ReturnDetails()
         {
-            return $"Name: {name}, Age: {age}";
+            return $"Name: {Name}, Age: {Age}";
         }
     }
     static void Main(string[] args)
@@ -28,8 +58,7 @@ class Program
         Person person = new Person("Alice", 30);
         Console.WriteLine(person.ReturnDetails());
 
-        person.SetName("Bob");
-        Console.WriteLine($"Your name is {person.GetName()}, and your age is {person.GetAge()}");
+        Console.WriteLine($"Your name is {person.Name}, and your age is {person.Age}");
 
 
         Console.ReadLine();
--- README.md
+++ README.md
@@ -1896,7 +1896,64 @@ The compiler always prioritizes the local variable over the global variable if t
 
 
 ## Class Properties
+    class Person
+    {
+        //Auto-implemented properties: when no additional logic is needed in the getter and setter
+        //public string Name { get; set; }
+        //public int Age { get; set; }
 
+        private string name;
+        private int age;
+
+        //Properties 
+        public string Name
+        {
+            /*
+            get 
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+             */
+            get => name;
+            set => name = !string.IsNullOrEmpty(value) ? value : "Invalid name";
+        }
+        public int Age
+        {
+            get => age;
+            set => age = value >= 0 && value <= 150 ? value : -1;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        /*
+        public void SetName(string name) => this.name = !string.IsNullOrEmpty(name) ? name : "Invalid name";
+        public string GetName() => name;
+        public void SetAge(int age) => this.age = age >= 0 && age <= 150 ? age : -1;
+        public int GetAge() => age;
+        */
+        
+        public string ReturnDetails()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+    static void Main(string[] args)
+    {
+        Person person = new Person("Alice", 30);
+        Console.WriteLine(person.ReturnDetails());
+
+        Console.WriteLine($"Your name is {person.Name}, and your age is {person.Age}");
+
+        Console.ReadLine();
+    }
 
 
 ## Class ToString Function Overrid
